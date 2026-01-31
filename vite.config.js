e70da9c8e9890d2128eee0458aad569c6146cd27
hector <hmh1988@hotmail.es>
2026-01-31 13:45:38 +0100

    css-3-modern-art-gallery

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    base: "/css-3-modern-art-gallery/",
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
+  },
+});
